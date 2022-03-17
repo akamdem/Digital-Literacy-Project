f2b39e40d0eeb7438beb60453e822aaa24945df4
akamdem <44042727+akamdem@users.noreply.github.com>
2022-03-17 04:03:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 import numpy as np
 import pickle
 import time
@@ -6,10 +7,12 @@ import time
 import json
 #import torch
 from collections import Counter
+image = Image.open('misinformation.png')
+st.image(image, caption='Combatting online misinformation')
 
-st.title("Hi there -- are you wondering if an online post/headline is credible? Airton Kamdem developed this machine learning web app to help guide you.")
-st.header("We'll make a prediction by comparing any given headline or text to thousands of other social media posts.")
-st.subheader("If the model classifies the given headline or post as something that would be observable in a credible news media post, it is likely trustworthy and will assign a percentage likelihood with its prediction, otherwise it'll  classify the text as 'conspiracy' and assign a similar probability.")
+st.header("Hi there -- are you wondering if an online post/headline is credible? Airton developed this web app to guide you.")
+st.subheader("Paste the text or headline below and we'll make a prediction by comparing any given headline or text to thousands of other social media posts.")
+#st.subheader("If the model classifies the given headline or post as something that would be observable in a credible news media post, it is likely trustworthy and will assign a percentage likelihood with its prediction, otherwise it'll  classify the text as 'conspiracy' and assign a similar probability.")
 
 
 
