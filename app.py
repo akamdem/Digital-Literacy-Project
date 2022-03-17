4e8cf3c9381b94b5a5527803c63315379851fa64
akamdem <44042727+akamdem@users.noreply.github.com>
2022-03-17 04:07:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,6 @@ st.header("Hi there -- are you wondering if an online post/headline is credible?
 st.subheader("Paste the text or headline below and we'll make a prediction by comparing any given headline or text to thousands of other social media posts.")
 #st.subheader("If the model classifies the given headline or post as something that would be observable in a credible news media post, it is likely trustworthy and will assign a percentage likelihood with its prediction, otherwise it'll  classify the text as 'conspiracy' and assign a similar probability.")
 
-
-
 with open("subreddit.pkl", "rb") as f:
     classifier = pickle.load(f)
 
